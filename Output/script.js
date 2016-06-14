6b9c65d7eaf345adb3b5be60f6d8d79383abb20e
kravenoff42 <jcraven42@gmail.com>
2016-06-13 23:47:41 -0400

    Moved Quote list to API, Automated excerpt

--- Output/script.js
+++ Output/script.js
@@ -3,38 +3,42 @@
 $(document).ready(function(){
   var currentQuote="";
 
-	$("#button").click(function(){
+	$("#button").click(function(currentQuote){
+    $.ajax({
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous',
+      type: 'POST', // The HTTP Method
+      data: {}, // Additional parameters here
+      datatype: 'json',
+      success: function(data) {
+        
+        var obj = $.parseJSON(data);
 
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-      $("#quote").text('"'+ a[0].content +'"');
-      $("#speaker").text('-'+ a[0].title +'(Sort of)');
-      currentQuote=a[0].content+' -'+ a[0].title +'(Sort of)';
-  });
-});
-/*    $.ajax({
-      //url: 'http://www.stands4.com/services/v2/quotes.php?uid=1001&tokenid=tk324324324&searchtype=RANDOM',
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback='
-      dataType: 'json',
-      type: 'get',
-      cache: true,
-      success: function(data){
-        console.log(data);
-        var quote = data[0].content;
-        var speaker = data[0].title;
-        $("#quote").text('"'+ quote +'"');
-        $("#speaker").text('-'+ speaker +'(Sort of)');
-        currentQuote=quote+' -'+ speaker +'(Sort of)';
+        var rawquote=obj.quote;
+
+        var quoteArr= rawquote.split(' ');
+        var chop= Math.floor(quoteArr.length/2.5);
+        var temp= quoteArr.slice(Math.floor(Math.random()*chop), quoteArr.length-Math.floor(Math.random()*chop));
+        var misquote=temp.join(' ');
+        $("#quote").text('"...'+misquote+'..."');
+        $("#speaker").text("-"+obj.author+' (Sort of)');
+        currentQuote='"...'+misquote+'..." -'+ obj.author +' (Sort of)';
+      },
+      error: function(err) { alert(err); },
+      beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-Mashape-Authorization', 'cqHXUJnBr2msh0tlL06vUMpjyTI3p1Cuqpyjsn48IBVtfWMBoT'); // Enter here your Mashape key
       }
-
+    });
+  });
+/*
       var item = Math.floor(Math.random()*47);
         $("#quote").text('"'+quotes.main[item].quote+'"');
         $("#speaker").text("-"+quotes.main[item].speaker+"(Sort of)");
         currentQuote='"'+quotes.main[item].quote+'" '+ quotes.main[item].speaker +'(Sort of)';
 
 
-    });
+
     */
-  });
+
   $("#link").click(function(){
       if (currentQuote!=""){
         $(this).attr("href", "http://twitter.com/home?status="+ currentQuote);
